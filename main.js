da6c1e66dc1c5c3db05e770d1163f9a79a484364
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2024-04-29 15:35:58 +0500

    end

--- main.js
+++ main.js
@@ -60,21 +60,98 @@
 // 19-misol
 // let n = prompt("Sonni kiriting: ") ;
 // let yigindi = 0;
-// let raqamlar = '';
+// let count=0;
 
 // while (n > 0) {
 //     let raqam = n % 10;
 //     yigindi += raqam;
-//     // raqamlar = raqam + (raqamlar !== '' ? ', ' : '') + raqamlar;
+//     n = Math.floor(n / 10);
+//     count++;
+// }
+// console.log("Raqamlar soni :"+count);
+// console.log("Yig'indi: "+ yigindi);
+// 18-misol
+// let n=prompt("sonni kiriting:" );
+// let teskariSon ="";
+
+// while (n > 0) {
+//     let raqam = n % 10;
+//     teskariSon += raqam;
 //     n = Math.floor(n / 10);
 // }
 
-// console.log("Raqamlar:", raqamlar);
-// console.log("Yig'indi:", yigindi);
+// console.log("Sonning raqamlari teskari tartibda: " + teskariSon);
+// 17-misol
+// let n=20;
+// let m=3;
+// let qoldiq=0;
+// let count=0;
+// while(n>=m){
+// n=n-m;
+// count++;
+// }
+// console.log("Butun:"+count);
+// console.log("Qoldiq:"+ n);
 
-if (A > B) {
-  for (let i = 0; i < A - B; i++) {
-      kesma += '-';
-  }
-  kesma += 'B';
+// 5-misol
+// let n=prompt("sonni kirit:")
+// let k = 0;
+// let temp = n;
+
+// while (temp % 2 === 0) {
+//     k++;
+//     temp = temp / 2;
+// }
+
+// if (temp === 1) {
+//     console.log(n+" soni 2 ning " + k +" chi  darajasi");
+// } else {
+//     console.log(n+" soni 2 ning" + k +" darajasi emas");
+// }
+// 4-misol
+// let n=prompt("sonni kirit:")
+// let k = 0;
+// let temp = n;
+
+// while (temp % 3 === 0) {
+//     k++;
+//     temp = temp / 3;
+// }
+
+// if (temp === 1) {
+//     console.log(n+" soni 3 ning darajasi");
+// } else {
+//     console.log(n+" soni 3 ning darajasi emas");
+// }
+
+// 3-misol
+
+// let n=16;
+// let k=3;
+// let qoldiq=0;
+// let count=0;
+// while(n>=k){
+// n=n-k;
+// count++;
+// }
+// console.log("Butun:"+count);
+// console.log("Qoldiq:"+ k);
+// 2-misol
+// let A = parseInt(prompt("Sonni kiriting: ") );
+// let B = parseInt(prompt("Sonni kiriting: ")) ;
+// let count=0;
+// while(A>=B){
+// A=A-B;
+// count++;
+// }
+// console.log("A kesmani B kesmada "+count+" marta joylashtirish mumkin");
+
+// 1-misol
+let A = parseInt(prompt("Sonni kiriting: ") );
+let B = parseInt(prompt("Sonni kiriting: ")) ;
+let count=0;
+while(A>=B){
+A=A-B;
+count++;
 }
+console.log("A kesmaning bo'sh qismi "+A+" ga teng");
